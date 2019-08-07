26d2f95a772b476d5350723d6897e3482adb164a
Max Holzheu <maxhm10@gmail.com>
2019-08-06 23:38:19 -0600

    finish skeleton of simulator

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "questions/part_2", as: :question_part_2
   get "questions/part_3", as: :question_part_3
 
-  get "simulator/start", as: :simulator
+  get "simulator/start", as: :simulator_start
   get "simulator/part_2", as: :simulator_part_2
   get "simulator/part_3", as: :simulator_part_3
 
