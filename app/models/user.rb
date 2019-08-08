a16ee7fac67e786da89d58ba68bb55410b02cc88
Max Holzheu <maxhm10@gmail.com>
2019-08-08 13:34:28 -0600

    add validation to prevent modifying unmodifiable attrs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,34 @@
 class User < ApplicationRecord
 
   validates :ip_address,
-            :edad,
-            :estado_residencia,
-            :ultimo_grado_cursado,
-            :sexo,
+            :age_id,
+            :residency_id,
+            :grade_id,
+            :sex,
             presence: true
 
-  validates :edad, numericality: { greater_than: 0, less_than: 100 }
-  validates :estado_residencia, inclusion: { in: Mexico::ESTADOS }
-  validates :ultimo_grado_cursado, inclusion: { in: Mexico::GRADOS_ESCOLARES }
-  validates :asistes_escuela, inclusion: [true, false]
+  validates :age_id,
+            :father_age_id,
+            :mother_age_id,
+            numericality: { greater_than_or_equal_to: 0, less_than: 100 }
+  validates :do_you_go_to_school, inclusion: [0, 1, 2]
+
+  validate :disallow_changing_values_after_create
+
+  private
+
+  def disallow_changing_values_after_create
+    if persisted? && (father_age_id_changed? ||
+      father_residency_id_changed? ||
+      father_grade_id_changed? ||
+      mother_age_id_changed? ||
+      mother_residency_id_changed? ||
+      mother_grade_id_changed? ||
+      age_id_changed? ||
+      residency_id_changed? ||
+      grade_id_changed?)
+      errors.add(:base, "Can't change attribute")
+    end
+  end
 
 end
