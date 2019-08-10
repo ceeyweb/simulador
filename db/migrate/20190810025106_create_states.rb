c838dabd2965fabe24619a980b0e99ff8d14a131
Gerardo Gaona <gerardo.gaona@gmail.com>
2019-08-10 15:31:12 -0500

    add 'State' model

---
+++ db/migrate/20190810025106_create_states.rb
@@ -0,0 +1,8 @@
+class CreateStates < ActiveRecord::Migration[5.2]
+  def change
+    create_table :states do |t|
+      t.string :description, null: false
+      t.references :region, null:false, foreign_key: true
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_10_024340) do
+ActiveRecord::Schema.define(version: 2019_08_10_025106) do
 
   create_table "regions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "description", null: false
+  end
+
+  create_table "states", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "description", null: false
+    t.bigint "region_id", null: false
+    t.index ["region_id"], name: "index_states_on_region_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -57,4 +63,5 @@ ActiveRecord::Schema.define(version: 2019_08_10_024340) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "states", "regions"
 end
