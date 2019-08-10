0ca0b5795c636712c832a58f18fe2167bc2e7fc0
Gerardo Gaona <gerardo.gaona@gmail.com>
2019-08-10 16:28:09 -0500

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,188 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-2.times do
-  User.create!(
-    ip_address: 1,
-    father_age_id: 1,
-    father_residency_id: 1,
-    father_grade_id: 1,
-    mother_age_id: 1,
-    mother_residency_id: 1,
-    mother_grade_id: 1,
-    age_id: 1,
-    residency_id: 1,
-    grade_id: 1,
-    sex: 1,
-    do_you_go_to_school: 1,
+# Source data
+REGIONS = [ # id, description
+  [1, "Norte"],
+  [2, "Norte Occidente"],
+  [3, "Centro Norte"],
+  [4, "Centro"],
+  [5, "Sur"],
+  [99, "No sabe/No aplica"]
+]
+
+STATES = [ # id, description, region_id
+  [1, "Aguascalientes", 3],
+  [2, "Baja California", 1],
+  [3, "Baja California Sur", 2],
+  [4, "Campeche", 5],
+  [5, "Coahuila", 1],
+  [6, "Colima", 3],
+  [7, "Chiapas", 5],
+  [8, "Chihuahua", 1],
+  [9, "Ciudad de México", 4],
+  [10, "Durango", 2],
+  [11, "Guanajuato", 4],
+  [12, "Guerrero", 5],
+  [13, "Hidalgo", 4],
+  [14, "Jalisco", 3],
+  [15, "Estado de México", 4],
+  [16, "Michoacán", 3],
+  [17, "Morelos", 4],
+  [18, "Nayarit", 2],
+  [19, "Nuevo León", 1],
+  [20, "Oaxaca", 5],
+  [21, "Puebla", 4],
+  [22, "Querétaro", 4],
+  [23, "Quintana Roo", 5],
+  [24, "San Luis Potosí", 3],
+  [25, "Sinaloa", 2],
+  [26, "Sonora", 1],
+  [27, "Tabasco", 5],
+  [28, "Tamaulipas", 1],
+  [29, "Tlaxcala", 4],
+  [30, "Veracruz", 5],
+  [31, "Yucatán", 5],
+  [32, "Zacatecas", 2],
+  [99, "No sabe/No aplica", 99]
+]
+
+EDUCATION_LEVELS = [ # id, description
+  [0, "Sin escolaridad"],
+  [1, "Primaria"],
+  [2, "Secundaria"],
+  [3, "Preparatoria"],
+  [4, "Universidad"],
+  [5, "Posgrado"],
+  [99, "No sabe/No aplica"]
+]
+
+EDUCATION_GRADES = [ # id, school_year, description, education_level_id
+  [1, 0, "Sin escolaridad", 0],
+  [2, 1, "1° Primaria", 1],
+  [3, 2, "2° Primaria", 1],
+  [4, 3, "3° Primaria", 1],
+  [5, 4, "4° Primaria", 1],
+  [6, 5, "5° Primaria", 1],
+  [7, 6, "6° Primaria", 1],
+  [8, 7, "1° Secundaria", 2],
+  [9, 8, "2° Secundaria", 2],
+  [10, 9, "3° Secundaria", 2],
+  [11, 10, "1° Preparatoria", 3],
+  [12, 11, "2° Preparatoria", 3],
+  [13, 12, "3° Preparatoria", 3],
+  [14, 13, "1° Universidad", 4],
+  [15, 14, "2° Universidad", 4],
+  [16, 15, "3° Universidad", 4],
+  [17, 16, "4° Universidad", 4],
+  [18, 17, "Universidad Completa", 4],
+  [19, 18, "1° Posgrado", 5],
+  [20, 19, "2° Posgrado", 5],
+  [21, 20, "3° Posgrado", 5],
+  [22, 21, "4° Posgrado", 5],
+  [23, 22, "5° Posgrado", 5],
+  [99, 0, "No sabe/No aplica", 99]
+]
+
+SEXES = [ # id, description
+  [1, "Mujer"],
+  [2, "Hombre"]
+]
+
+JOB_TYPES = [ # id, description
+  [1,	"Jornalero"],
+  [2,	"Empleado"],
+  [3,	"Patrón"],
+  [4,	"Trabajador por cuenta propia"],
+  [99, "No sabe/No aplica"],
+]
+
+JOB_SECTORS = [ # id, description
+  [1,	"Primario"],
+  [2,	"Secundario"],
+  [3,	"Servicios"],
+  [99, "No sabe/No aplica"]
+]
+
+JOB_EMPLOYEES_GROUPS = [ # id, description
+  [1, "1 a 10"],
+  [2, "11 a 50"],
+  [3, "Más de 50"],
+  [99, "No sabe/No aplica"]
+]
+
+JOB_SCHEDULES = [ # id, description
+  [1, "Tiempo completo (40 horas semanales o más)"],
+  [2, "Medio tiempo (menos de 40 horas semanales)"],
+  [99, "No sabe/No aplica"]
+]
+
+AGE_GROUPS = [ # id, description, lower_limit, upper_limit
+  [1, "6 a 11 años", 6, 11],
+  [2, "12 a 14 años", 12, 14],
+  [3, "15 a 17 años", 15, 17],
+  [4, "18 a 23 años", 18, 23],
+  [5, "Más de 23 años", 24, 120]
+]
+
+# Clean tables - keep specified order, otherwise DB foreign key restriction
+# errors may occur
+State.delete_all
+Region.delete_all
+EducationGrade.delete_all
+EducationLevel.delete_all
+Sex.delete_all
+JobType.delete_all
+JobSector.delete_all
+JobEmployeesGroup.delete_all
+JobSchedule.delete_all
+AgeGroup.delete_all
+
+# Load data - keep specified order, otherwise DB foreign key restriction
+# errors may occur
+REGIONS.each do |id, description|
+  Region.create!(id: id, description: description)
+end
+
+STATES.each do |id, description, region_id|
+  State.create!(id: id, description: description, region_id: region_id)
+end
+
+EDUCATION_LEVELS.each do |id, description|
+  EducationLevel.create!(id: id, description: description)
+end
+
+EDUCATION_GRADES.each do |id, year, description, education_level_id|
+  EducationGrade.create!(
+    id: id,
+    school_year: year,
+    description: description,
+    education_level_id: education_level_id
+  )
+end
+
+SEXES.each do |id, description|
+  Sex.create!(id: id, description: description)
+end
+
+JOB_TYPES.each do |id, description|
+  JobType.create!(id: id, description: description)
+end
+
+JOB_SECTORS.each do |id, description|
+  JobSector.create!(id: id, description: description)
+end
+
+JOB_EMPLOYEES_GROUPS.each do |id, description|
+  JobEmployeesGroup.create!(id: id, description: description)
+end
+
+JOB_SCHEDULES.each do |id, description|
+  JobSchedule.create!(id: id, description: description)
+end
+
+AGE_GROUPS.each do |id, description, lower_limit, upper_limit|
+  AgeGroup.create!(
+    id: id,
+    description: description,
+    lower_limit: lower_limit,
+    upper_limit: upper_limit
   )
 end
