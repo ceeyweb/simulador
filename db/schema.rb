a8f5124e29cab273a47e3087630381dc972660f7
Gerardo Gaona <gerardo.gaona@gmail.com>
2019-08-10 15:25:49 -0500

    add 'Region' model

--- db/schema.rb
+++ db/schema.rb
@@ -10,37 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_192337) do
+ActiveRecord::Schema.define(version: 2019_08_10_024340) do
+
+  create_table "regions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "description", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "ip_address", null: false
-    t.integer "father_age_id", null: false
-    t.integer "father_residency_id", null: false
-    t.integer "father_grade_id", null: false
-    t.integer "mother_age_id", null: false
-    t.integer "mother_residency_id", null: false
-    t.integer "mother_grade_id", null: false
-    t.integer "age_id", null: false
-    t.integer "residency_id", null: false
-    t.integer "grade_id", null: false
-    t.integer "sex", null: false
-    t.integer "do_you_go_to_school", null: false
-    t.integer "father_is_employed"
-    t.integer "father_has_healthcare"
-    t.integer "father_job_type"
-    t.integer "father_has_employees"
-    t.integer "mother_is_employed"
-    t.integer "mother_has_healthcare"
-    t.integer "mother_job_type"
-    t.integer "mother_has_employees"
-    t.integer "is_employed"
-    t.integer "is_first_job"
-    t.integer "has_healthcare"
-    t.integer "job_type_id"
-    t.integer "has_employees"
-    t.integer "job_time_id"
-    t.integer "job_sector_id"
-    t.integer "how_many_employees_id"
+    t.integer "edad", null: false
+    t.string "estado_residencia", null: false
+    t.string "ultimo_grado_cursado", null: false
+    t.integer "sexo", null: false
+    t.boolean "asistes_escuela", null: false
+    t.integer "actualmente_trabajas"
+    t.integer "padre_trabajaba"
+    t.integer "cuenta_con_servicios_medicos"
+    t.integer "cuenta_con_servicios_medicos_padre"
+    t.integer "tipo_de_trabajo"
+    t.integer "tipo_de_trabajo_padre"
+    t.integer "tiene_empleados"
+    t.integer "tiene_empleados_padre"
+    t.integer "cuantos_empleados"
+    t.integer "cuantos_empleados_padre"
+    t.integer "tiempo_trabajo"
+    t.integer "tiempo_trabajo_padre"
+    t.integer "sector_empleo"
+    t.integer "sector_empleo_padre"
     t.integer "en_hogar_habia_celular"
     t.integer "en_hogar_habia_telefono_fijo"
     t.integer "en_hogar_habia_internet"
@@ -51,6 +47,12 @@ ActiveRecord::Schema.define(version: 2019_07_20_192337) do
     t.integer "en_hogar_habia_agua"
     t.integer "en_hogar_habia_bano"
     t.integer "en_hogar_habia_electricidad"
+    t.integer "edad_padre"
+    t.string "estado_residencia_padre"
+    t.string "ultimo_grado_cursado_padre"
+    t.integer "edad_madre"
+    t.string "estado_residencia_madre"
+    t.string "ultimo_grado_cursado_madre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
