f3ca6123c87f97991577d1d775df6f2ea814541b
Gerardo Gaona <gerardo.gaona@gmail.com>
2019-08-13 22:50:03 -0500

    add kpis data loader

---
+++ lib/csv_loader.rb
@@ -0,0 +1,46 @@
+require "csv"
+
+class CSVLoader
+
+  def self.load(*args)
+    new(*args).load
+  end
+
+  def initialize(model)
+    @model = model
+  end
+
+  def load
+    model.delete_all
+    model.connection.execute(insert)
+    model.table_name
+  end
+
+  private
+
+  attr_reader :model
+
+  def insert
+    <<~SQL.squish
+      INSERT INTO `#{model.table_name}` (`#{columns.join('`, `')}`)
+      VALUES #{values.join(', ')}
+    SQL
+  end
+
+  def values
+    CSV.foreach(file, headers: true).map do |row|
+      "(#{row.to_hash.values.join(',')})"
+    end
+  end
+
+  def file
+    Rails.root.join("db", "data", "#{model.table_name}.csv")
+  end
+
+  def columns
+    CSV.open(file, "r") do |csv|
+      csv.first
+    end
+  end
+
+end
---
+++ lib/tasks/data_load.rake
@@ -0,0 +1,25 @@
+require Rails.root.join("lib", "csv_loader")
+
+namespace :data do
+  namespace :health do
+
+    desc "Load all health related 'Kpis' tables"
+    task load_all: :environment do
+      puts "Loading tables..."
+
+      [
+        Kpis::LifeExpectancy,
+        Kpis::LifeExpectancyCountry,
+        Kpis::LifeExpectancyRegion,
+        Kpis::LifeExpectancyWorld
+      ].each do |model|
+        puts " > #{model.table_name}"
+
+        CSVLoader.load(model)
+      end
+
+      puts "...complete!"
+    end
+
+  end
+end
