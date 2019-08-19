1920e844b544488cb3132450ee5dd6739c040130
Gerardo Gaona <gerardo.gaona@gmail.com>
2019-08-19 08:22:34 -0500

    add 'select' to tertile filter (optimize query)

--- app/models/kpis/education_achievement_tertile_filter.rb
+++ app/models/kpis/education_achievement_tertile_filter.rb
@@ -1,6 +1,7 @@
 module Kpis::EducationAchievementTertileFilter
   def tertile_for(achievement)
-    order(tertile: :DESC).
+    select(:tertile).
+      order(tertile: :DESC).
       find_by(
         "`lower_limit` <= ? AND `upper_limit` >= ?",
         achievement,
