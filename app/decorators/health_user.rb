d6b0e93a1230f19d95e208a9e668467a13be89fa
Gerardo Gaona <gerardo.gaona@gmail.com>
2019-08-23 11:01:01 -0500

    update 'HealthUser' decorator

--- app/decorators/health_user.rb
+++ app/decorators/health_user.rb
@@ -1,7 +1,53 @@
 class HealthUser < SimpleDelegator
 
-  def health_results
-    HealthResults.new(__getobj__)
+  include ActionView::Helpers::NumberHelper
+  include ActionView::Helpers::AssetTagHelper
+
+  SEX_TEXT = {
+    Sex::FEMALE_ID => "las mujeres", Sex::MALE_ID => "los hombres"
+  }.freeze
+
+  HealthResults.instance_methods(false).each do |method|
+    define_method(method) do
+      number_with_precision(
+        results.public_send(method),
+        precision: 0,
+      )
+    end
+  end
+
+  def region
+    user.region.description
+  end
+
+  def expected_age_image
+    "esperanza_vida_#{sex.downcase}.png"
+  end
+
+  def region_map_image
+    "mapa_region_#{region.tr(" ", "_").downcase}.png"
+  end
+
+  def sex
+    user.sex.description
+  end
+
+  def sex_text
+    SEX_TEXT[user.sex_id]
+  end
+
+  def education_level
+    user.education_level.id
+  end
+
+  private
+
+  def user
+    __getobj__
+  end
+
+  def results
+    @results ||= HealthResults.new(user)
   end
 
 end
