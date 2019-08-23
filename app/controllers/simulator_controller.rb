6c21dfd5c3c9f6d40e48679ab6d1fc89637ac915
Gerardo Gaona <gerardo.gaona@gmail.com>
2019-08-23 11:06:11 -0500

    connect health results page with back

--- app/controllers/simulator_controller.rb
+++ app/controllers/simulator_controller.rb
@@ -1,12 +1,19 @@
 class SimulatorController < ApplicationController
 
   def start
+    @user = HealthUser.new(user)
   end
 
   def part_2
   end
 
   def part_3
+  end
+
+  private
+
+  def user
+    User.find(cookies["user_id"])
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def simulator_user_image(education_level, user_education_level, sex, section)
+    if user_education_level >= education_level
+      image_tag("#{sex.downcase}_con_birrete_#{section}.png", class: "grad-image")
+    else
+      image_tag(
+        "#{sex.downcase}_sin_birrete.png",
+        id: "education-level-#{education_level}",
+        class: "grad-image",
+        style: "margin-top: 26px;",
+      )
+    end
+  end
 end
