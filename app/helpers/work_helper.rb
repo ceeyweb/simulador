1b584f55ebc08b34cd73347900e12a0d82b2fffa
Max Holzheu <maxhm10@gmail.com>
2019-10-04 13:55:44 -0500

    add tooltip to work results

---
+++ app/helpers/work_helper.rb
@@ -0,0 +1,5 @@
+module WorkHelper
+  def tooltip_text
+    "La formalidad laboral es el acceso a la seguridad social, la cual tiene importantes beneficios para los trabajadores, como la afiliación a servicios de salud, obtención de préstamos de vivienda y cotización a un sistema de pensiones."
+  end
+end
