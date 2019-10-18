abfe6d483f46ee45e4a50d7b0e3ec855806e50c9
Max Holzheu <maxhm10@gmail.com>
2019-10-18 13:28:16 -0500

    new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_13_140101) do
+ActiveRecord::Schema.define(version: 2019_10_18_182643) do
 
   create_table "age_groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "description", null: false
@@ -170,10 +170,6 @@ ActiveRecord::Schema.define(version: 2019_10_13_140101) do
     t.integer "father_has_healthcare"
     t.bigint "father_job_type_id"
     t.bigint "father_job_employees_group_id"
-    t.integer "mother_is_employed"
-    t.integer "mother_has_healthcare"
-    t.bigint "mother_job_type_id"
-    t.bigint "mother_job_employees_group_id"
     t.boolean "is_employed"
     t.boolean "is_first_job"
     t.boolean "has_healthcare"
@@ -206,8 +202,6 @@ ActiveRecord::Schema.define(version: 2019_10_13_140101) do
     t.index ["job_sector_id"], name: "index_users_on_job_sector_id"
     t.index ["job_type_id"], name: "index_users_on_job_type_id"
     t.index ["mother_education_grade_id"], name: "index_users_on_mother_education_grade_id"
-    t.index ["mother_job_employees_group_id"], name: "index_users_on_mother_job_employees_group_id"
-    t.index ["mother_job_type_id"], name: "index_users_on_mother_job_type_id"
     t.index ["mother_residency_id"], name: "index_users_on_mother_residency_id"
     t.index ["residency_id"], name: "index_users_on_residency_id"
     t.index ["sex_id"], name: "index_users_on_sex_id"
@@ -234,9 +228,7 @@ ActiveRecord::Schema.define(version: 2019_10_13_140101) do
   add_foreign_key "users", "education_grades", column: "father_education_grade_id"
   add_foreign_key "users", "education_grades", column: "mother_education_grade_id"
   add_foreign_key "users", "job_employees_groups", column: "father_job_employees_group_id"
-  add_foreign_key "users", "job_employees_groups", column: "mother_job_employees_group_id"
   add_foreign_key "users", "job_types", column: "father_job_type_id"
-  add_foreign_key "users", "job_types", column: "mother_job_type_id"
   add_foreign_key "users", "states", column: "father_residency_id"
   add_foreign_key "users", "states", column: "mother_residency_id"
   add_foreign_key "users", "states", column: "residency_id"
