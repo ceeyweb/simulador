4f82f2fe50ed5d854026c4d3b4788e25ac6bfc8a
Gerardo Gaona <gerardo.gaona@gmail.com>
2019-10-21 19:57:01 -0500

    add Docker configuration files

---
+++ .docker/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid files
+rm -f /app/tmp/pids/server.pid
+
+# Execute migrations
+bundle exec rake db:migrate
+
+# Precompile assets
+bundle exec rake assets:precompile
+
+# Execite container's main process
+exec "$@"
