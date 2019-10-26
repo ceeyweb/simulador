bd33ed2dafe9315c1864d0130b9bb1965024d963
Gerardo Gaona <gerardo.gaona@gmail.com>
2019-10-26 14:29:08 -0500

    prevent simulator navigation if data is incomplete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,18 +2,23 @@ class QuestionsController < ApplicationController
 
   def start
     return redirect_to where_user_left_off unless is_new_user?
+
     @user = User.new
   end
 
   def part_2
     return redirect_to :question_start if is_new_user?
+
     @user = ProgressUser.new(user)
+
     redirect_to :question_part_3 if @user.part_2_is_complete?
   end
 
   def part_3
     return redirect_to :question_start if is_new_user?
+
     @user = ProgressUser.new(user)
+
     redirect_to :simulator_start if @user.filled_out_all_parts?
   end
 
--- app/controllers/simulator_controller.rb
+++ app/controllers/simulator_controller.rb
@@ -1,16 +1,25 @@
 class SimulatorController < ApplicationController
 
   def start
+    return redirect_to :question_start if is_new_user?
+
     @user = HealthUser.new(ProgressUser.new(user))
   end
 
   def part_2
+    return redirect_to :question_start if is_new_user?
+
     @user = EducationUser.new(ProgressUser.new(user))
+
+    redirect_to :simulator_start unless @user.filled_out_all_parts?
   end
 
   def part_3
+    return redirect_to :question_start if is_new_user?
+
     @user = WorkUser.new(ProgressUser.new(user))
 
+    return redirect_to :simulator_start unless @user.filled_out_all_parts?
     render :user_is_employed if @user.is_employed?
   end
 
