c9df2d7c291cc58a526cebee68007e1cbc2f58dd
Gerardo Gaona <gerardo.gaona@gmail.com>
2019-10-28 16:31:31 -0600

    update education achievement formula

--- app/kpis/education_achievement.rb
+++ app/kpis/education_achievement.rb
@@ -1,6 +1,8 @@
 class EducationAchievement
 
   PRESCHOOL_YEARS = 5
+  MAX_TSE = 14
+  TSE_AGE_LIMIT = 24
 
   def initialize(age, school_year)
     @age = age
@@ -8,11 +10,17 @@ class EducationAchievement
   end
 
   def value
-    [school_year / (age - PRESCHOOL_YEARS).to_f, 1].min * 100
+    [school_year / tse.to_f, 1].min * 100
   end
 
   private
 
   attr_reader :age, :school_year
+
+  def tse
+    return MAX_TSE if age > TSE_AGE_LIMIT
+
+    age - PRESCHOOL_YEARS
+  end
 
 end
