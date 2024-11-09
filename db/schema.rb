1e7954dd69433f1fbe7e597a262581ce10a161fa
Gerardo Gaona <gerardo.gaona@gmail.com>
2024-11-09 13:49:14 -0600

    Add/Update rails 6 configuration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_155851) do
+ActiveRecord::Schema.define(version: 2024_11_09_194133) do
 
   create_table "administrators", charset: "utf8", force: :cascade do |t|
     t.string "username", null: false
